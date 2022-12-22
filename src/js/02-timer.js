0fe23594cd6901e19f6da376b2d6db38de1ac44e
OPaslavskiy <bankir.op@gmail.com>
2022-12-22 20:32:01 +0200

    up

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,14 +3,18 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 // const inputDate = document.querySelector('#datetime-picker');
 
+let selectedDate = null;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    selectedDate = selectedDates[0];
+    console.log(selectedDate);
   },
 };
 
 flatpickr('#datetime-picker', options);
+console.log(selectedDate);
