265501bb7e09360853ce1a0e46607d0c40e60d26
OPaslavskiy <bankir.op@gmail.com>
2022-12-23 16:37:54 +0200

    2 task. ready

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,9 +1,19 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-// const inputDate = document.querySelector('#datetime-picker');
+let selectedDate;
+let difference;
+let timer = {};
+let currentDate;
 
-let selectedDate = null;
+const startBtn = document.querySelector('button[data-start]');
+
+const refs = {
+  daysTimer: document.querySelector('span[data-days]'),
+  hoursTimer: document.querySelector('span[data-hours]'),
+  minutesTimer: document.querySelector('span[data-minutes]'),
+  secondsTimer: document.querySelector('span[data-seconds]'),
+};
 
 const options = {
   enableTime: true,
@@ -12,9 +22,67 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     selectedDate = selectedDates[0];
-    console.log(selectedDate);
   },
 };
 
+startBtn.addEventListener('click', choiseDate);
+
 flatpickr('#datetime-picker', options);
-console.log(selectedDate);
+
+function choiseDate() {
+  currentDate = new Date();
+  if (currentDate - selectedDate < 0) {
+    lunchTimer();
+    return;
+  }
+  alert('Please choose a date in the future');
+}
+
+function lunchTimer() {
+  const timerID = setInterval(() => {
+    currentDate = new Date();
+    difference = selectedDate - currentDate;
+    timer = convertMs(difference);
+    console.log(difference);
+    addLeadingZero(timer);
+    if (difference < 1000) {
+      stopTimer(timerID);
+    }
+  }, 1000);
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(timer) {
+  const zeroTimer = {
+    days: timer.days.toString().padStart(2, '0'),
+    hours: timer.hours.toString().padStart(2, '0'),
+    minutes: timer.minutes.toString().padStart(2, '0'),
+    seconds: timer.seconds.toString().padStart(2, '0'),
+  };
+
+  pageTimerUpdates(zeroTimer, refs);
+}
+
+function pageTimerUpdates(zeroTimer, refs) {
+  // const abv = timer.days.padStart(2, '0');
+  refs.daysTimer.textContent = zeroTimer.days;
+  refs.hoursTimer.textContent = zeroTimer.hours;
+  refs.minutesTimer.textContent = zeroTimer.minutes;
+  refs.secondsTimer.textContent = zeroTimer.seconds;
+}
+
+function stopTimer(timerID) {
+  clearInterval(timerID);
+}
